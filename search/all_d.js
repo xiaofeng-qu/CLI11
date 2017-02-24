14a945764cdcdbfc56211f0642285c2a88f7e1af
Travis CI <travis@travis-ci.org>
2017-02-24 17:56:42 +0000

    Deploy code docs to GitHub Pages Travis build: 188
    Commit: ade78c5842640965506b7e74b654ccee224288b2

--- search/all_d.js
+++ search/all_d.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['range',['Range',['../group__validator__group.html#gae30c14787933ba22e7f4b36d0eeac172',1,'CLI::Range(T min, T max)'],['../group__validator__group.html#ga3c9d28f65c8120540e1865cf78d65ece',1,'CLI::Range(T max)']]],
   ['remove_5foption',['remove_option',['../class_c_l_i_1_1_app.html#a3058b128735eec0813589b56c5453115',1,'CLI::App']]],
-  ['require_5fsubcommand',['require_subcommand',['../class_c_l_i_1_1_app.html#adc3abfe0f615fa1d11b26e417acb4846',1,'CLI::App']]],
+  ['require_5fsubcommand',['require_subcommand',['../class_c_l_i_1_1_app.html#acdb3e002977fc5edbb5042c7aad7dd3d',1,'CLI::App']]],
   ['required',['required',['../class_c_l_i_1_1_option.html#a950395187e95aec210928b36e90621bb',1,'CLI::Option']]],
+  ['required_5fsubcommand',['required_subcommand',['../class_c_l_i_1_1_app.html#a2eab120acff2150ec0fb7adaaf65e9ea',1,'CLI::App']]],
   ['requirederror',['RequiredError',['../struct_c_l_i_1_1_required_error.html',1,'CLI']]],
   ['requires',['requires',['../class_c_l_i_1_1_option.html#abee0e47f0df529a1d0b2cc9ed2ca33d9',1,'CLI::Option::requires(Option *opt)'],['../class_c_l_i_1_1_option.html#a6edf22002aebe6e0ac69c61283be4ec5',1,'CLI::Option::requires(Option *opt, Option *opt1, ARG... args)']]],
   ['requireserror',['RequiresError',['../struct_c_l_i_1_1_requires_error.html',1,'CLI']]],
