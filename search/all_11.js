0d10fba5f5592946bd888ed23fdf4de686254703
Travis CI <travis@travis-ci.org>
2017-03-30 17:50:35 +0000

    Deploy code docs to GitHub Pages Travis build: 313
    Commit: 49d1a5da1fe38a5bcb3ebe829d62b7eaa6d39636

--- search/all_11.js
+++ search/all_11.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['time_5fit',['time_it',['../class_c_l_i_1_1_timer.html#a7e078926ba12b6529177be001357f2e7',1,'CLI::Timer']]],
   ['time_5fpoint',['time_point',['../class_c_l_i_1_1_timer.html#a3a5c183e3fdeacd3056aa694d9c82aec',1,'CLI::Timer']]],
   ['time_5fprint_5f',['time_print_',['../class_c_l_i_1_1_timer.html#a098d182ddc9880d20f993a6a990847c2',1,'CLI::Timer']]],
   ['time_5fprint_5ft',['time_print_t',['../class_c_l_i_1_1_timer.html#a58eec081edc2366d759c693966d4f1a0',1,'CLI::Timer']]],
--- search/functions_f.js
+++ search/functions_f.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['time_5fit',['time_it',['../class_c_l_i_1_1_timer.html#a7e078926ba12b6529177be001357f2e7',1,'CLI::Timer']]],
   ['timer',['Timer',['../class_c_l_i_1_1_timer.html#a1cad535ef02042b51906cb464861c9be',1,'CLI::Timer']]],
   ['to_5fstring',['to_string',['../class_c_l_i_1_1_timer.html#a35a7a90b7f96da40dfe4cbfb2f0b4f9e',1,'CLI::Timer']]]
 ];
