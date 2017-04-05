c45714a8154d142cbc6ddc053b627744b10bd6c3
Travis CI <travis@travis-ci.org>
2017-04-05 13:37:04 +0000

    Deploy code docs to GitHub Pages Travis build: 322
    Commit: e779d15ef8d3f62a131050b4609b591634000813

--- search/all_7.js
+++ search/all_7.js
@@ -1,12 +1,12 @@
 var searchData=
 [
   ['get_5fcallback_5frun',['get_callback_run',['../class_c_l_i_1_1_option.html#aae19e4231b0c1ef14cf2f3c7189e453c',1,'CLI::Option']]],
-  ['get_5fconfig_5fptr',['get_config_ptr',['../class_c_l_i_1_1_app.html#aa516fbfd33a220af66deccd8e6a465a0',1,'CLI::App']]],
+  ['get_5fconfig_5fptr',['get_config_ptr',['../class_c_l_i_1_1_app.html#aa516fbfd33a220af66deccd8e6a465a0',1,'CLI::App::get_config_ptr()'],['../class_c_l_i_1_1_app.html#aa3108b9777788e910610454f06ecfaf6',1,'CLI::App::get_config_ptr() const']]],
   ['get_5fdefault',['get_default',['../class_c_l_i_1_1_option.html#a26527442a386c8ccc369069c00062398',1,'CLI::Option']]],
   ['get_5fdescription',['get_description',['../class_c_l_i_1_1_option.html#ad79474a3c1cc8a73b12f80c6e31ee3af',1,'CLI::Option']]],
   ['get_5fexpected',['get_expected',['../class_c_l_i_1_1_option.html#a307543e6e4ddeb6e4ea00438b5b10be3',1,'CLI::Option']]],
   ['get_5fgroup',['get_group',['../class_c_l_i_1_1_option.html#a5f6adbac10f12a3865e94d6ad59f2a83',1,'CLI::Option']]],
-  ['get_5fhelp_5fptr',['get_help_ptr',['../class_c_l_i_1_1_app.html#ae3ed738a07047fd1d76c228d41804a76',1,'CLI::App']]],
+  ['get_5fhelp_5fptr',['get_help_ptr',['../class_c_l_i_1_1_app.html#ae3ed738a07047fd1d76c228d41804a76',1,'CLI::App::get_help_ptr()'],['../class_c_l_i_1_1_app.html#ad3b89865119fcdaf213880e25f7cff87',1,'CLI::App::get_help_ptr() const']]],
   ['get_5fname',['get_name',['../class_c_l_i_1_1_app.html#a4b40e301840ec5d6f99129e6d0a0b2e9',1,'CLI::App::get_name()'],['../class_c_l_i_1_1_option.html#a21524238e17fd0367f4a3528665e7816',1,'CLI::Option::get_name()']]],
   ['get_5fpositional',['get_positional',['../class_c_l_i_1_1_option.html#acab7033604b49e314d290b01adea690d',1,'CLI::Option']]],
   ['get_5frequired',['get_required',['../class_c_l_i_1_1_option.html#a1b1aaa271902bca28a2c526d015a93c1',1,'CLI::Option']]],
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['add_5fcomplex',['add_complex',['../class_c_l_i_1_1_app.html#a8ac22d0401df5f801c1c77af1e5f6ef1',1,'CLI::App']]],
   ['add_5fconfig',['add_config',['../class_c_l_i_1_1_app.html#a7ca58da6a7d884e09cd964d5544eabd1',1,'CLI::App']]],
   ['add_5fflag',['add_flag',['../class_c_l_i_1_1_app.html#a58e4d1ac98afdb75a0a689adcb6b173f',1,'CLI::App::add_flag(std::string name, std::string description=&quot;&quot;)'],['../class_c_l_i_1_1_app.html#ad3608e288902be51227ce8549bac6743',1,'CLI::App::add_flag(std::string name, T &amp;count, std::string description=&quot;&quot;)'],['../class_c_l_i_1_1_app.html#ad3608e288902be51227ce8549bac6743',1,'CLI::App::add_flag(std::string name, T &amp;count, std::string description=&quot;&quot;)']]],
   ['add_5foption',['add_option',['../class_c_l_i_1_1_app.html#a6e8e118fcc2a06f9d21c6b3d84bb0af4',1,'CLI::App::add_option(std::string name, callback_t callback, std::string description=&quot;&quot;, bool defaulted=false)'],['../class_c_l_i_1_1_app.html#a5ba8a993d48f76f98674193e77835bc6',1,'CLI::App::add_option(std::string name, T &amp;variable, std::string description=&quot;&quot;, bool defaulted=false)'],['../class_c_l_i_1_1_app.html#a0c65e4b0ad917b965e39d2e8f590bb53',1,'CLI::App::add_option(std::string name, std::vector&lt; T &gt; &amp;variable, std::string description=&quot;&quot;, bool defaulted=false)']]],
