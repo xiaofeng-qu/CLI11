885b00fc64af23646ccd3bb0903e5820d50c8e5d
Travis CI <travis@travis-ci.org>
2017-11-20 15:17:23 +0000

    Deploy code docs to GitHub Pages Travis build: 517
    Commit: 79cd791fb7ff30b76b976c51dfc03efb1b825208

--- search/all_6.js
+++ search/all_6.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['fallthrough',['fallthrough',['../class_c_l_i_1_1_app.html#a2e32bb0c9270c996488cc8dfa28ec8b1',1,'CLI::App']]],
   ['fallthrough_5f',['fallthrough_',['../class_c_l_i_1_1_app.html#ad3196989217de9162ffa0a5c1e81e92c',1,'CLI::App']]],
-  ['fileerror',['FileError',['../struct_c_l_i_1_1_file_error.html',1,'CLI']]]
+  ['fileerror',['FileError',['../struct_c_l_i_1_1_file_error.html',1,'CLI']]],
+  ['footer_5f',['footer_',['../class_c_l_i_1_1_app.html#a4fbf8b55a33e24d4c5a0532568dd7302',1,'CLI::App']]]
 ];
--- search/variables_4.js
+++ search/variables_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['fallthrough_5f',['fallthrough_',['../class_c_l_i_1_1_app.html#ad3196989217de9162ffa0a5c1e81e92c',1,'CLI::App']]]
+  ['fallthrough_5f',['fallthrough_',['../class_c_l_i_1_1_app.html#ad3196989217de9162ffa0a5c1e81e92c',1,'CLI::App']]],
+  ['footer_5f',['footer_',['../class_c_l_i_1_1_app.html#a4fbf8b55a33e24d4c5a0532568dd7302',1,'CLI::App']]]
 ];
