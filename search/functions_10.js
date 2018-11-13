8f77c1c4dc68f8708262d3d6eff785ebf2ea0f90
Travis CI <travis@travis-ci.org>
2018-11-13 16:17:29 +0000

    Deploy code docs to GitHub Pages Travis build: 1064
    Commit: a78f5bcdcf8d34f5191c01856445a256497b4a7f

--- search/functions_10.js
+++ search/functions_10.js
@@ -3,6 +3,8 @@ var searchData=
   ['range',['Range',['../struct_c_l_i_1_1_range.html#acdc2184cbc527fed68c9500c6aae15be',1,'CLI::Range::Range(T min, T max)'],['../struct_c_l_i_1_1_range.html#a83d910e7a9faf8e73af6f8a8fd70bf69',1,'CLI::Range::Range(T max)']]],
   ['remaining',['remaining',['../class_c_l_i_1_1_app.html#a0474d71afa5672f645d2b645723c9818',1,'CLI::App']]],
   ['remaining_5fsize',['remaining_size',['../class_c_l_i_1_1_app.html#a32adb7bd2643ff7e293802210516ca95',1,'CLI::App']]],
+  ['remove_5fexcludes',['remove_excludes',['../class_c_l_i_1_1_option.html#a414e6a8f76443036a9fb096f49165660',1,'CLI::Option']]],
+  ['remove_5fneeds',['remove_needs',['../class_c_l_i_1_1_option.html#aab72f7fef9aca5eefb25b09c1abffb3c',1,'CLI::Option']]],
   ['remove_5foption',['remove_option',['../class_c_l_i_1_1_app.html#a3058b128735eec0813589b56c5453115',1,'CLI::App']]],
   ['require_5fsubcommand',['require_subcommand',['../class_c_l_i_1_1_app.html#a9cb090791a2594ffca877fd6afbf858a',1,'CLI::App::require_subcommand()'],['../class_c_l_i_1_1_app.html#ae1bb42a7f768d636671552cb345f2d6c',1,'CLI::App::require_subcommand(int value)'],['../class_c_l_i_1_1_app.html#a07b1b36c8362df966ba8f16428b1e7da',1,'CLI::App::require_subcommand(size_t min, size_t max)']]],
   ['required',['required',['../class_c_l_i_1_1_option_base.html#a38c0ea47532de712dd8d9803633383b2',1,'CLI::OptionBase']]],
--- search/variables_a.js
+++ search/variables_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['name_5f',['name_',['../class_c_l_i_1_1_app.html#a61a36f7297a3199b9e789887510fa3fa',1,'CLI::App']]]
+  ['name_5f',['name_',['../class_c_l_i_1_1_app.html#a61a36f7297a3199b9e789887510fa3fa',1,'CLI::App']]],
+  ['needs_5f',['needs_',['../class_c_l_i_1_1_option.html#a3bd8d2ff461e8046b284cdbfcd741dce',1,'CLI::Option']]]
 ];
