f2987c291839bb97c0e93a4265b9031bb7e51e55
Travis CI <travis@travis-ci.org>
2019-01-22 17:26:18 +0000

    Deploy code docs to GitHub Pages Travis build: 1121
    Commit: 8d7aefe21f24d8aa77e19eced2ce93d806fffa92

--- search/all_4.js
+++ search/all_4.js
@@ -4,5 +4,6 @@ var searchData=
   ['default_5fstr',['default_str',['../class_c_l_i_1_1_option.html#a446f47a5d6ddf6831defd707394d7c07',1,'CLI::Option']]],
   ['default_5fval',['default_val',['../class_c_l_i_1_1_option.html#a30f91bad482e7f60a70f465722c203c6',1,'CLI::Option']]],
   ['defaultval_5f',['defaultval_',['../class_c_l_i_1_1_option.html#af5801b4cec3c87ce84a2cfd9decdb1fd',1,'CLI::Option']]],
+  ['description',['description',['../class_c_l_i_1_1_app.html#a33abaeded5281c702ee6e4382539bfd8',1,'CLI::App::description()'],['../class_c_l_i_1_1_option.html#a646cab3be78b2821f0f545fc53d4c2c1',1,'CLI::Option::description()']]],
   ['description_5f',['description_',['../class_c_l_i_1_1_app.html#aa6a2e79e0a5990b44bc5d76504437fcd',1,'CLI::App::description_()'],['../class_c_l_i_1_1_option.html#aa6427b2f3303cc4469cacdc098b9d4bf',1,'CLI::Option::description_()']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['default_5fstr',['default_str',['../class_c_l_i_1_1_option.html#a446f47a5d6ddf6831defd707394d7c07',1,'CLI::Option']]],
-  ['default_5fval',['default_val',['../class_c_l_i_1_1_option.html#a30f91bad482e7f60a70f465722c203c6',1,'CLI::Option']]]
+  ['default_5fval',['default_val',['../class_c_l_i_1_1_option.html#a30f91bad482e7f60a70f465722c203c6',1,'CLI::Option']]],
+  ['description',['description',['../class_c_l_i_1_1_app.html#a33abaeded5281c702ee6e4382539bfd8',1,'CLI::App::description()'],['../class_c_l_i_1_1_option.html#a646cab3be78b2821f0f545fc53d4c2c1',1,'CLI::Option::description()']]]
 ];
